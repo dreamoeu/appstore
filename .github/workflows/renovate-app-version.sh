0213b9f8aaa4836772cf0981ec71427f00b9c05c
Meng Sen <qyg2297248353@gmail.com>
2024-12-16 10:51:57 +0800

    测试更新脚本
    Signed-off-by: Meng Sen <qyg2297248353@gmail.com>

--- .github/workflows/renovate-app-version.sh
+++ .github/workflows/renovate-app-version.sh
@@ -88,10 +88,8 @@ for docker_compose_file in $docker_compose_files
 do
     echo "Processing docker-compose file: $docker_compose_file"
 
-    # 判断 app_name 是否在关联数组中
     if [[ -n "${app_keys[$app_name]}" ]]; then
         echo "$app_name is in the app_keys list."
-        # 获取对应的 keys
         IFS=' ' read -r -a keys <<< "${app_keys[$app_name]}"
         for key in "${keys[@]}"; do
             echo "Processing key: $key"
@@ -101,17 +99,15 @@ do
                 image=$(yq .services.$first_service.image $docker_compose_file)
                 echo "Found image for service $first_service: $image"
 
-                # 进行后续处理
                 process_image "$image" "$app_name"
             else
                 echo "Key $key not found in $docker_compose_file"
             fi
         done
 
-        # 在循环完毕后执行版本移动操作
         if [[ -n "$cleaned_version" ]]; then
             mv apps/$app_name/$old_version apps/$app_name/$cleaned_version
-            echo "Moved $old_version to $cleaned_version"
+            echo "Moved apps/$app_name/$old_version to apps/$app_name/$cleaned_version"
         else
             echo "Could not determine version for image: $image"
         fi
@@ -122,13 +118,11 @@ do
         image=$(yq .services.$first_service.image $docker_compose_file)
         echo "Found image: $image"
 
-        # 进行后续处理
         process_image "$image" "$app_name"
 
-        # 在处理第一个服务后执行版本移动操作
         if [[ -n "$cleaned_version" ]]; then
             mv apps/$app_name/$old_version apps/$app_name/$cleaned_version
-            echo "Moved $old_version to $cleaned_version"
+            echo "Moved apps/$app_name/$old_version to apps/$app_name/$cleaned_version"
         else
             echo "Could not determine version for image: $image"
         fi
