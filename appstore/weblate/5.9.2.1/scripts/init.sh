2620babab01543de16de2ffea3b201f8d8271d73
QYG2297248353 <QYG2297248353@users.noreply.github.com>
2025-01-09 09:19:55 +0000

    Processed apps directory via GitHub Actions

--- appstore/weblate/5.9.2.1/scripts/init.sh
+++ appstore/weblate/5.9.2.1/scripts/init.sh
@@ -10,6 +10,15 @@ if [ -f .env ]; then
   echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
   echo "GLOBAL_ENV_FILE=${CURRENT_DIR}/envs/global.env" >> .env
 
+  # setup-2 update dir permissions
+  mkdir -p "$WEBLATE_ROOT_PATH"
+  mkdir -p "$WEBLATE_ROOT_PATH/data"
+  mkdir -p "$WEBLATE_ROOT_PATH/cache"
+
+  chown -R 1000:1000 "$WEBLATE_ROOT_PATH"
+  chown -R 1000:1000 "$WEBLATE_ROOT_PATH/data"
+  chown -R 1000:1000 "$WEBLATE_ROOT_PATH/cache"
+
   echo "Check Finish."
 
 else
