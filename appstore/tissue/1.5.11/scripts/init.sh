935584ad1083f06da0fd8c02c46f573c60156978
QYG2297248353 <QYG2297248353@users.noreply.github.com>
2025-01-24 06:26:14 +0000

    Processed apps directory via GitHub Actions

--- appstore/tissue/1.5.11/scripts/init.sh
+++ appstore/tissue/1.5.11/scripts/init.sh
@@ -7,20 +7,8 @@ if [ -f .env ]; then
   CURRENT_DIR=$(pwd)
   sed -i '/^ENV_FILE=/d' .env
   sed -i '/^GLOBAL_ENV_FILE=/d' .env
-  sed -i '/^APP_ENV_FILE=/d' .env
   echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
   echo "GLOBAL_ENV_FILE=${CURRENT_DIR}/envs/global.env" >> .env
-  echo "APP_ENV_FILE=${CURRENT_DIR}/envs/outline.env" >> .env
-
-  # setup-2 remove empty values
-  sed -i '/^.*=""/d' .env
-
-  # setup-3 update directory permissions
-  mkdir -p "$OUTLINE_ROOT_PATH"
-  mkdir -p "$OUTLINE_ROOT_PATH/data"
-
-  chown -R 1001:1001 "$OUTLINE_ROOT_PATH"
-  chmod -R 1777 "$OUTLINE_ROOT_PATH"
 
   echo "Check Finish."
 
