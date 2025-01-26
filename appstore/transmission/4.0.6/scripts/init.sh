2d830ddc2a1c64f311bacf2f320945f0da76ad08
QYG2297248353 <QYG2297248353@users.noreply.github.com>
2025-01-26 09:17:44 +0000

    Processed apps directory via GitHub Actions

--- appstore/transmission/4.0.6/scripts/init.sh
+++ appstore/transmission/4.0.6/scripts/init.sh
@@ -10,20 +10,19 @@ if [ -f .env ]; then
   echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
   echo "GLOBAL_ENV_FILE=${CURRENT_DIR}/envs/global.env" >> .env
 
-  mkdir -p "$TRANSMISSION_ROOT_PATH"
-  mkdir -p "$TRANSMISSION_ROOT_PATH/config"
   mkdir -p "$TRANSMISSION_ROOT_PATH/downloads"
-  mkdir -p "$TRANSMISSION_ROOT_PATH/watch"
+  mkdir -p "$TRANSMISSION_ROOT_PATH/downloads/complete"
+  mkdir -p "$TRANSMISSION_ROOT_PATH/downloads/incomplete"
 
   # setup-2 install theme
-  if [ ! -d "$TRANSMISSION_ROOT_PATH/ui/themes" ]; then
-    mkdir -p "$TRANSMISSION_ROOT_PATH/ui/themes"
+  if [ ! -d "$TRANSMISSION_ROOT_PATH/themes" ]; then
+    mkdir -p "$TRANSMISSION_ROOT_PATH/themes"
 
     if command -v unzip >/dev/null 2>&1; then
-      unzip -o themes/flood-for-transmission.zip -d "$TRANSMISSION_ROOT_PATH/ui/themes"
+      unzip -o themes/flood-for-transmission.zip -d "$TRANSMISSION_ROOT_PATH/themes"
       UNZIP_SUCCESS=$?
     elif command -v tar >/dev/null 2>&1; then
-      tar -xf themes/flood-for-transmission.zip -C "$TRANSMISSION_ROOT_PATH/ui/themes"
+      tar -xf themes/flood-for-transmission.zip -C "$TRANSMISSION_ROOT_PATH/themes"
       UNZIP_SUCCESS=$?
     else
       echo "Warning: Neither 'unzip' nor 'tar' is installed. Skipping theme extraction."
@@ -34,9 +33,9 @@ if [ -f .env ]; then
       echo "Theme successfully extracted."
 
       if grep -q "^TRANSMISSION_WEB_HOME=" .env; then
-          sed -i 's|^TRANSMISSION_WEB_HOME=.*|TRANSMISSION_WEB_HOME=/ui/themes/flood-for-transmission|' .env
+          sed -i 's|^TRANSMISSION_WEB_HOME=.*|TRANSMISSION_WEB_HOME=/themes/flood-for-transmission|' .env
       else
-        echo "TRANSMISSION_WEB_HOME=/ui/themes/flood-for-transmission" >> .env
+        echo "TRANSMISSION_WEB_HOME=/themes/flood-for-transmission" >> .env
       fi
     else
       echo "Warning: Failed to extract the theme archive. Proceeding without extracted theme."
@@ -44,6 +43,8 @@ if [ -f .env ]; then
   else
     echo "Themes directory already exists. Skipping extraction."
   fi
+
+  chown -R 1000:1000 "$TRANSMISSION_ROOT_PATH"
 
   echo "Check Finish."
 
