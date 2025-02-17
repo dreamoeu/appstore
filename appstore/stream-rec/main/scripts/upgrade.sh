bccd51347cc1793f25bba275d8335a4a689e6c23
QYG2297248353 <QYG2297248353@users.noreply.github.com>
2025-02-17 03:25:11 +0000

    Processed apps directory via GitHub Actions

--- appstore/stream-rec/main/scripts/upgrade.sh
+++ appstore/stream-rec/main/scripts/upgrade.sh
@@ -10,11 +10,6 @@ if [ -f .env ]; then
   echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
   echo "GLOBAL_ENV_FILE=${CURRENT_DIR}/envs/global.env" >> .env
 
-  mkdir -p "$GLANCE_ROOT_PATH"
-  mkdir -p "$GLANCE_ROOT_PATH/app"
-
-  cp ./conf/glance.yml "$GLANCE_ROOT_PATH/app/glance.yml"
-
   echo "Check Finish."
 
 else
--- appstore/super-productivity/11.1.3/scripts/init.sh
+++ appstore/super-productivity/11.1.3/scripts/init.sh
@@ -10,10 +10,11 @@ if [ -f .env ]; then
   echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
   echo "GLOBAL_ENV_FILE=${CURRENT_DIR}/envs/global.env" >> .env
 
-  mkdir -p "$GLANCE_ROOT_PATH"
-  mkdir -p "$GLANCE_ROOT_PATH/app"
+  mkdir -p "$SUPER_PRODUCTIVITY_ROOT_PATH"
+  mkdir -p "$SUPER_PRODUCTIVITY_ROOT_PATH/data"
+  mkdir -p "$SUPER_PRODUCTIVITY_ROOT_PATH/config"
 
-  cp ./conf/glance.yml "$GLANCE_ROOT_PATH/app/glance.yml"
+  cp ./conf/webdav.yaml "$SUPER_PRODUCTIVITY_ROOT_PATH/config/webdav.yaml"
 
   echo "Check Finish."
 
