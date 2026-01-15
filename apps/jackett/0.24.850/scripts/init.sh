4ab97df5db2acee0acac953e03c9d2b889828ebb
ms <qyg2297248353@qq.com>
2026-01-15 15:59:04 +0800

    发布应用 Jackett

---
+++ apps/jackett/0.24.850/scripts/init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ -f .env ]; then
+  source .env
+
+  # setup-1 add default values
+  CURRENT_DIR=$(pwd)
+  sed -i '/^ENV_FILE=/d' .env
+  sed -i '/^GLOBAL_ENV_FILE=/d' .env
+  echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
+  echo "GLOBAL_ENV_FILE=${CURRENT_DIR}/envs/global.env" >> .env
+
+  # setup-2 create dir
+  mkdir -p "$JACKETT_ROOT_PATH"
+  mkdir -p "$JACKETT_ROOT_PATH/config"
+  mkdir -p "$JACKETT_ROOT_PATH/downloads"
+
+  chown -R 1000:1000 "$JACKETT_ROOT_PATH"
+
+  echo "Check Finish."
+
+else
+  echo "Error: .env file not found."
+fi
