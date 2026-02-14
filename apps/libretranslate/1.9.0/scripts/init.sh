78e5b9597262c851029c26778903c2b3ad7c30f7
ms <qyg2297248353@gmail.com>
2026-02-15 02:43:21 +0800

    更新

--- apps/libretranslate/1.9.0/scripts/init.sh
+++ apps/libretranslate/1.9.0/scripts/init.sh
@@ -10,6 +10,8 @@ if [ -f .env ]; then
   echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
   echo "GLOBAL_ENV_FILE=${CURRENT_DIR}/envs/global.env" >> .env
 
+  chown -R 1032:65534 "$LIBRETRANSLATE_ROOT_PATH"
+
   echo "Check Finish."
 
 else
