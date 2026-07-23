24f920f76ae31115ac0603d3276c6a1c4cf3d45c
QYG2297248353 <QYG2297248353@users.noreply.github.com>
2026-07-23 06:06:48 +0000

    Processed apps directory via GitHub Actions

--- appstore/omniroute/3.8.48/scripts/init.sh
+++ appstore/omniroute/3.8.48/scripts/init.sh
@@ -10,6 +10,10 @@ if [ -f .env ]; then
   echo "ENV_FILE=${CURRENT_DIR}/.env" >> .env
   echo "GLOBAL_ENV_FILE=${CURRENT_DIR}/envs/global.env" >> .env
 
+  mkdir -p "$OMNIROUTE_ROOT_PATH"
+  mkdir -p "$OMNIROUTE_ROOT_PATH/omniroute"
+  chown -R 1000:1000 "$OMNIROUTE_ROOT_PATH"
+
   echo "Check Finish."
 
 else
